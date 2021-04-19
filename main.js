9230c8657c490bbeb76aaca7a77671657edd64d8
Nild13 <76243167+Nild13@users.noreply.github.com>
2021-04-19 10:53:39 +0200

    edit js with modal

--- main.js
+++ main.js
@@ -111,6 +111,6 @@ document.getElementById('search').onclick = function() {
 };
 document.querySelector('.testClass').addEventListener('click', function(){
     console.log('blah')
-    let getEdit_data = JSON.parse(localStorage('data'))
+    let getEdit_data = JSON.parse(localStorage.getItem('data'))
     console.log(getEdit_data)
 })
